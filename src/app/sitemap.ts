b85781e3a7543bc28692ac2a2d57dc024cb79ed9
oleksandr <smiyanaleksandr@gmail.com>
2026-03-06 12:36:57 +0200

    edit site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const BASE_URL = 'https://yourname.dev';
+const BASE_URL = 'https://smiianoleksandr.vercel.app/';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
